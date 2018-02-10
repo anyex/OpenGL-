fddbc8892adaaa07d2d980e1a967c3f4a2292526
anyex <1174932183@qq.com>
2018-02-10 21:04:24 +0800

    简单的变换操作，平移，旋转等

--- OpenGL/OpenGL/3DFunction.cpp
+++ OpenGL/OpenGL/3DFunction.cpp
@@ -1,5 +1,6 @@
 #include "3DFunction.h"
-
+float move_s = 0.0f;
+float route_angle = 0;
 void SetupMatrices(int w, int h)
 {
 	glMatrixMode(GL_PROJECTION);
@@ -25,22 +26,24 @@ void InitOpenGL()
 
 void Render()
 {
-	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);//清除当前缓冲区的值
 
 	glLoadIdentity();
-	gluLookAt(0.0f, 0.0f, 0.0f,
-		0.0f, 0.0f, -100.0f,
-		0.0f, 1.0f, 0.0f);//改变观察变换
+	gluLookAt(0.0f, 0.0f, 2.0f,
+		0.0f, -0.0f, 1.0f,
+		0.0f, -1.0f, 0.0f);//改变观察变换，这里将会得到一个倒立的图像
 
+	
+	glPushMatrix();//使用矩阵堆栈就只对下面的一个点进行了平移
+	//glTranslatef(0.5f, 0, 0);
 	//绘制点
-
 	glPointSize(15.0f);//设置点的大小
 	glBegin(GL_POINTS);//打开要绘制点的功能
 	glColor3f(1.0f, 0.0f, 0.0f);//设定颜色
 	glVertex3f(0.0f, 1.0f, -5.0f);//设置点的位置
-
-	glEnd();//绘制点结束
 	
+	glEnd();//绘制点结束
+	glPopMatrix();
 	if (!glIsEnabled(GL_POINT_SMOOTH))//检测是否打开抗锯齿性能
 	
 		glEnable(GL_POINT_SMOOTH);
@@ -74,14 +77,16 @@ void Render()
 
 		glBegin(GL_LINES);
 		glColor3f(0.0f, 0.0f, 1.0f);
-		glVertex3f(-1.0f, -1.5f, -5.0f);
-		glVertex3f(2.0f, -1.5f, -5.0f);
+		glVertex3f(-6.0f+ move_s, -1.5f, -5.0f);//动态平移虚线
+		glVertex3f(-4.0f+ move_s, -1.5f, -5.0f);
 		glEnd();
 		glDisable(GL_LINE_STIPPLE);
 
 
 		//绘制三角形
+
 		glTranslatef(0.0f, 0.0f, -5.0f);
+		glRotatef(route_angle, 0.0f, 0.0f, 1.0f);//旋转三角形
 		if (!glIsEnabled(GL_POLYGON_SMOOTH))
 		{
 			glEnable(GL_POLYGON_SMOOTH);
@@ -97,6 +102,10 @@ void Render()
 		glVertex3f(2.0f, -1.0f, 0.0f);
 
 		glEnd();
-
-
+		move_s += 0.04f;
+		route_angle += 0.1f;
+		if (move_s >=8)
+		{
+			move_s = 0;
+		}
 }
