db93f82f0bd86242f75e641f69bc6f292c2027ad
anyex <1174932183@qq.com>
2018-02-11 19:56:34 +0800

    刻画一个机器人

--- OpenGL/OpenGL/3DFunction.cpp
+++ OpenGL/OpenGL/3DFunction.cpp
@@ -1,6 +1,11 @@
 #include "3DFunction.h"
+
+
 float move_s = 0.0f;
 float route_angle = 0;
+float armAngle[2] = { 0 };
+float legAngle[2] = { 0 };
+float angle = 0;
 void SetupMatrices(int w, int h)
 {
 	glMatrixMode(GL_PROJECTION);
@@ -29,12 +34,12 @@ void Render()
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);//清除当前缓冲区的值
 
 	glLoadIdentity();
-	gluLookAt(0.0f, 0.0f, 2.0f,
-		0.0f, -0.0f, 1.0f,
-		0.0f, -1.0f, 0.0f);//改变观察变换，这里将会得到一个倒立的图像
+	gluLookAt(0.0f, .0f, 2.0f,
+		0.0f, 0.0f, 1.0f,
+		0.0f, 1.0f, 3.0f);//改变观察变换，这里将会得到一个倒立的图像
 
 	
-	glPushMatrix();//使用矩阵堆栈就只对下面的一个点进行了平移
+	/*glPushMatrix();//使用矩阵堆栈就只对下面的一个点进行了平移
 	//glTranslatef(0.5f, 0, 0);
 	//绘制点
 	glPointSize(15.0f);//设置点的大小
@@ -101,11 +106,222 @@ void Render()
 		glColor3f(0.0f, 0.0f, 1.0f);
 		glVertex3f(2.0f, -1.0f, 0.0f);
 
-		glEnd();
-		move_s += 0.04f;
-		route_angle += 0.1f;
-		if (move_s >=8)
-		{
-			move_s = 0;
-		}
+		glEnd();*/
+		
+	//绘制机器人
+	glPushMatrix();
+	glTranslatef(0.0f, 0.0f, -30.f);
+	glRotatef(angle, 0.0f, 1.0f, 0.0f);
+	RenderRobert(0.0f, 0.0f, 0.0f);
+	glPopMatrix();
+
+	angle = angle + 0.05f;
+
+	if (angle >= 360.0f)
+	{
+		angle = 0.0f;
+	}
 }
+
+//画一个机器人
+void RenderRobert(float xPos,float yPos,float zPos)
+{
+	static bool leg1 = true;
+	static bool leg2 = false;
+
+	static bool arm1 = true;
+	static bool arm2 = false;
+
+	glPushMatrix();
+		glTranslatef(xPos, yPos, zPos);
+		DrawHead(1.0f, 2.0f, 0.0f);
+		DrawTorso(1.5f, 0.0f, 0.0f);
+	glPushMatrix();
+		if (arm1)
+		
+			armAngle[0] = armAngle[0] + 0.1f;
+		else
+			armAngle[0] = armAngle[0] - 0.1f;
+
+		//如果胳膊达到其最大的角度则改变其状态
+		if (armAngle[0]>=15.0f)
+		{
+			arm1 = false;
+		}
+		if (armAngle[0]<=-15.0f)
+		{
+			arm1 = true;
+		}
+
+		//平移并旋转后绘制胳膊
+		glTranslatef(0.0f, -0.5f, 0.0f);
+		glRotatef(armAngle[0], 1.0f, 0.0f, 0.0f);
+		DrawArm(2.5f, 0.0f, -0.5f);
+	glPopMatrix();
+		
+
+	glPushMatrix();
+	glTranslatef(xPos, yPos, zPos);
+	DrawHead(1.0f, 2.0f, 0.0f);
+	DrawTorso(1.5f, 0.0f, 0.0f);
+	glPushMatrix();
+	if (arm2)
+
+		armAngle[1] = armAngle[1] + 0.1f;
+	else
+		armAngle[1] = armAngle[1] - 0.1f;
+
+	//如果胳膊达到其最大的角度则改变其状态
+	if (armAngle[1] >= 15.0f)
+	{
+		arm2 = false;
+	}
+	if (armAngle[1] <= -15.0f)
+	{
+		arm2 = true;
+	}
+
+	//平移并旋转后绘制胳膊
+	glTranslatef(0.0f, -0.5f, 0.0f);
+	glRotatef(armAngle[1], 1.0f, 0.0f, 0.0f);
+	DrawArm(-1.5f, 0.0f, -0.5f);
+	glPopMatrix();
+
+	//绘制腿部
+
+	glPushMatrix();
+	if (leg1)
+	{
+		legAngle[0] = legAngle[0] + 0.1f;
+	}
+	else
+		legAngle[0] = legAngle[0] - 0.1f;
+
+	if (legAngle[0]>=15.0f)
+	{
+		leg1 = false;
+	}
+	if (legAngle[0]<=-15.0f)
+	{
+		leg1 = true;
+	}
+
+	glTranslatef(0.0f, -0.5f, 0.0f);
+	glRotatef(legAngle[0], 1.0f, 0.0f, 0.0f);
+	DrawLeg(-0.5f, -0.5f, -0.5f);
+
+	glPopMatrix();
+
+	glPushMatrix();
+	if (leg2)
+	{
+		legAngle[1] = legAngle[1] + 0.1f;
+	}
+	else
+		legAngle[1] = legAngle[1] - 0.1f;
+
+	if (legAngle[1]>=15.0f)
+	{
+		leg2 = false;
+	}
+	
+	if (legAngle[1]<=-15.0f)
+	{
+		leg2 = true;
+	}
+
+	glTranslatef(0.0f, -0.5f, 0.0f);
+	glRotatef(legAngle[1], 1.0f, 0.0f, 0.0f);
+	DrawLeg(1.5f, -5.0f, -0.5f);
+
+	glPopMatrix();
+
+
+
+}
+//绘制立方体
+void DrawCube(float xPos, float yPos, float zPos)
+{
+	glPushMatrix();
+	glTranslatef(xPos, yPos, zPos);
+
+	glBegin(GL_POLYGON);
+	//顶面
+	glVertex3f(0.0f, 0.0f, 0.0f);
+	glVertex3f(-1.0f, 0.0f, 0.0f);
+	glVertex3f(-1.0f, -1.0f, 0.0f);
+	glVertex3f(0.0f, -1.0f, 0.0f);
+	//前面
+	glVertex3f(0.0f, 0.0f, 0.0f);
+	glVertex3f(-1.0f, 0.0f, 0.0f);
+	glVertex3f(-1.0f, -1.f, 0.0f);
+	glVertex3f(0.0f, -1.0f, 0.0f);
+	//右面
+	glVertex3f(0.0f, 0.0f, 0.0f);
+	glVertex3f(0.0f, -1.0f, 0.0f);
+	glVertex3f(0.0f, -1.0f, -1.0f);
+	glVertex3f(0.0f, 0.0f, -1.0f);
+	//左面
+	glVertex3f(-1.0f, 0.0f, 0.0f);
+	glVertex3f(-1.0f, 0.0f, -1.0f);
+	glVertex3f(-1.0f, -1.0f, -1.0f);
+	glVertex3f(-1.0f, -1.0f, 0.0f);
+	//底面
+	glVertex3f(0.0f, 0.0f, 0.0f);
+	glVertex3f(-1.0f, 0.0f, -1.0f);
+	glVertex3f(-1.0f, -1.0f, -1.0f);
+	glVertex3f(0.0f, -1.0f, -1.0f);
+	//后面
+	glVertex3f(0.0f, 0.0f, 0.0f);
+	glVertex3f(-1.0f, 0.0f, -1.0f);
+	glVertex3f(-1.0f, -1.0f, -1.0f);
+	glVertex3f(0.0f, -1.0f, -1.0f);
+
+	
+
+	glEnd();
+
+
+}
+
+void DrawArm(float xPos, float yPos, float zPos)
+{
+	glPushMatrix();
+		glColor3f(1.0f, 0.0f, 0.0f);
+		glTranslatef(xPos, yPos, zPos);
+		glScalef(1.0f, 4.0f, 1.0f);
+		DrawCube(0.0f, 0.0f, 0.f);
+	glPushMatrix();
+}
+
+void DrawHead(float xPos, float yPos, float zPos)
+{
+	glPushMatrix();
+		glColor3f(1.0f, 1.0f, 1.0f);
+		glTranslatef(xPos, yPos, zPos);
+		glScalef(2.0f, 2.0f, 2.0f);
+		DrawCube(0.0f, 0.0f, 0.0f);
+	glPopMatrix();
+}
+
+void DrawTorso(float xPos, float yPos, float zPos)
+{
+	glPushMatrix();
+		glColor3f(0.0f, 0.0f, 1.0f);
+		glTranslatef(xPos, yPos, zPos);
+		glScalef(3.0f, 5.0f, 2.0f);
+		DrawCube(0.0f, 0.0f, 0.0f);
+	glPushMatrix();
+}
+
+void DrawLeg(float xPos, float yPos, float zPos)
+{
+	glPushMatrix();
+		glColor3f(1.0f, 1.0f, 0.0f);
+		glTranslatef(xPos, yPos, zPos);
+		glScalef(1.0f, 5.0f, 1.0f);
+		DrawCube(0.0f, 0.0f, 0.0f);
+	glPopMatrix();
+}
+
+
--- OpenGL/OpenGL/3DFunction.h
+++ OpenGL/OpenGL/3DFunction.h
@@ -7,4 +7,9 @@
 void SetupMatrices(int w, int h);
 void InitOpenGL();
 void Render();
-
+void DrawCube(float xPos, float yPos, float zPos);
+void DrawArm(float xPos, float yPos, float zPos);
+void RenderRobert(float xPos, float yPos, float zPos);
+void DrawLeg(float xPos, float yPos, float zPos);
+void DrawTorso(float xPos, float yPos, float zPos);
+void DrawHead(float xPos, float yPos, float zPos);
